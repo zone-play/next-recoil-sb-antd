e34ee991e9d7784e5fc4a91b27c8ca3878ec3730
DanaCoding <modana@protonmail.com>
2021-11-30 02:09:16 +0800

    优化count方法

--- components/minus.component.tsx
+++ components/minus.component.tsx
@@ -4,9 +4,10 @@ import { changeCount } from '../recoil/count.recoil';
 export default function () {
 
     const [minusCount, setMinusCount] = useRecoilState(changeCount('-'));
+    const minusFunc = () => setMinusCount(minusCount);
 
     return (
-        <button onClick={() => setMinusCount(minusCount)}>-</button>
+        <button onClick={minusFunc}>-</button>
     )
 
 }
--- components/plus.component.tsx
+++ components/plus.component.tsx
@@ -3,10 +3,11 @@ import { changeCount } from '../recoil/count.recoil';
 
 export default function () {
 
-    const [plusCount, setPlusCount] = useRecoilState(changeCount('+'))
+    const [plusCount, setPlusCount] = useRecoilState(changeCount('+'));
+    const plusFunc = () => setPlusCount(plusCount);
 
     return (
-        <button onClick={() => setPlusCount(plusCount)}>+</button>
+        <button onClick={plusFunc}>+</button>
     )
 
 }
