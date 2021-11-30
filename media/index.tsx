2319cfdd8ec80b4bee64e963698f590a29ec1078
DanaCoding <modana@protonmail.com>
2021-12-01 03:45:07 +0800

    响应式布局

---
+++ media/index.tsx
@@ -0,0 +1,14 @@
+import { createMedia } from '@artsy/fresnel';
+
+const ExampleAppMedia = createMedia({
+  breakpoints: {
+    sm: 0,
+    md: 768,
+    lg: 1024,
+  },
+});
+
+// Make styles for injection into the header of the page
+export const mediaStyles = ExampleAppMedia.createMediaStyle();
+
+export const { Media, MediaContextProvider } = ExampleAppMedia;
